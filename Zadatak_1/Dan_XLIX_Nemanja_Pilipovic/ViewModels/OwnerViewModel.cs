51565df14f858a477ba2531c57d16282bfec2aa2
Trbuhozborac <nemanjapipi@hotmail.com>
2020-08-04 12:36:35 +0200

    Imported Models from DB and Add Button to the Owner View

--- Zadatak_1/Dan_XLIX_Nemanja_Pilipovic/ViewModels/OwnerViewModel.cs
+++ Zadatak_1/Dan_XLIX_Nemanja_Pilipovic/ViewModels/OwnerViewModel.cs
@@ -1,12 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dan_XLIX_Nemanja_Pilipovic.Commands;
+using Dan_XLIX_Nemanja_Pilipovic.Views;
+using System.Windows.Input;
 
 namespace Dan_XLIX_Nemanja_Pilipovic.ViewModels
 {
-    class OwnerViewModel
+    public class OwnerViewModel : BaseViewModel
     {
+        #region Objects
+
+        OwnerView main;
+
+        #endregion
+
+        #region Constructors
+
+        public OwnerViewModel(OwnerView mainOpen)
+        {
+            main = mainOpen;
+        }
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Commands
+
+        private ICommand createStaff;
+        public ICommand CreateStaff
+        {
+            get
+            {
+                if (createStaff == null)
+                {
+                    createStaff = new RelayCommand(param => CreateStaffExecute(), param => CanCreateStaff());
+                }
+                return createStaff;
+            }
+        }
+
+        private ICommand createManager;
+        public ICommand CreateManager
+        {
+            get
+            {
+                if (createManager == null)
+                {
+                    createManager = new RelayCommand(param => CreateManagerExecute(), param => CanCreateManager());
+                }
+                return createManager;
+            }
+        }
+
+        #endregion
+
+        #region Functions
+
+        private void CreateStaffExecute()
+        {
+
+        }
+
+        private bool CanCreateStaff()
+        {
+            return true;
+        }
+
+        private void CreateManagerExecute()
+        {
+
+        }
+
+        private bool CanCreateManager()
+        {
+            return true;
+        }
+
+        #endregion
     }
 }
