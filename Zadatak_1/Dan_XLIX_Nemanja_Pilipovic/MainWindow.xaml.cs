8dfed4a9b5bd62b8fedaa5a20a54469f2040fcd1
Trbuhozborac <nemanjapipi@hotmail.com>
2020-08-04 12:27:40 +0200

    Added Database

--- Zadatak_1/Dan_XLIX_Nemanja_Pilipovic/MainWindow.xaml.cs
+++ Zadatak_1/Dan_XLIX_Nemanja_Pilipovic/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Dan_XLIX_Nemanja_Pilipovic.Views;
+using System.IO;
+using System.Windows;
 
 namespace Dan_XLIX_Nemanja_Pilipovic
 {
@@ -12,9 +14,35 @@ namespace Dan_XLIX_Nemanja_Pilipovic
             InitializeComponent();
         }
 
+        private readonly string _location = @"~\..\..\..\OwnerAccess.txt";
+        private string _ownerUsername;
+        private string _ownerPassword;
+
+        private void GetOwnerCredentials()
+        {
+            if (File.Exists(_location))
+            {
+                string[] allLines = File.ReadAllLines(_location);
+                string[] usernameLine = allLines[0].Split(':');
+                string[] passwordLine = allLines[1].Split(':');
+
+                _ownerUsername = usernameLine[1];
+                _ownerPassword = passwordLine[1];
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("File not Found");
+            }
+        }
+
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
-
+            GetOwnerCredentials();
+            if(txtUsername.Text == _ownerUsername && txtPassword.Password == _ownerPassword)
+            {
+                OwnerView view = new OwnerView();
+                view.ShowDialog();
+            }            
         }
     }
 }
